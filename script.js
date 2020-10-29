768e105f4bc35dd24ee63fa9c7b752b3d8d21c62
Jonghyun Kang <misterjaykay@gmail.com>
2020-10-29 17:19:23 -0400

    change layout, made it one page portfolio

--- script.js
+++
@@ -1,9 +0,0 @@
-var update = setInterval( function() {  
-    var localTime  = moment.utc().toDate();
-    var currentTime = moment(localTime).format("h:mm:ss a");
-    var currentDate = moment(localTime).format("MMM Do YYYY");
-    $(".date-goes-here").text(currentDate);
-    $(".time-goes-here").text(currentTime);
-
-}, 1000)
-setInterval(update, 1000);
